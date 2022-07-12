f20c78194e5df20a588cdcc029916aec829f1444
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2022-07-12 13:55:56 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import torch
 from torchvision import models
 model = models.resnet18(weights=ResNet18_Weights.DEFAULT)
-torch.save(model.state_dict(), "torchserve-demo/resnet18.pth")
+torch.save(model.state_dict(), "resnet18.pth")
 torchscripted_model = torch.jit.script(model)
 torchscripted_model.save('resnet18.pt')
